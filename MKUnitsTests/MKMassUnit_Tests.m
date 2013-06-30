f8278e25885e56560adb5eecac6c6dff66461891
Michal Konturek <michal.konturek@gmail.com>
2013-06-30 02:59:37 +0100

    MKMassUnit passes tests.

---
+++ MKUnitsTests/MKMassUnit_Tests.m
@@ -0,0 +1,52 @@
+//
+//  MKMassUnit_Tests.m
+//  MKUnits
+//
+//  Created by Michal Konturek on 30/06/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import "Base_Tests.h"
+
+@interface MKMassUnit_Tests : SenTestCase
+
+@end
+
+@implementation MKMassUnit_Tests
+
+- (void)test_one_gram_is_1000_miligrams {
+    MKQuantity *expected = [MKQuantity mass_milligramWithAmount:@1000];
+    BOOL result = [[MKQuantity mass_gramWithAmount:@1]isTheSame:expected];
+    
+    assertThatBool(result, equalToBool(YES));
+}
+
+- (void)test_one_decagram_is_10_grams {
+    MKQuantity *expected = [MKQuantity mass_gramWithAmount:@10];
+    BOOL result = [[MKQuantity mass_decagramWithAmount:@1]isTheSame:expected];
+    
+    assertThatBool(result, equalToBool(YES));
+}
+
+- (void)test_one_kilogram_is_1000_grams {
+    MKQuantity *expected = [MKQuantity mass_gramWithAmount:@1000];
+    BOOL result = [[MKQuantity mass_kilogramWithAmount:@1]isTheSame:expected];
+    
+    assertThatBool(result, equalToBool(YES));
+}
+
+- (void)test_one_megagram_is_1000_killograms {
+    MKQuantity *expected = [MKQuantity mass_kilogramWithAmount:@1000];
+    BOOL result = [[MKQuantity mass_megagramWithAmount:@1]isTheSame:expected];
+    
+    assertThatBool(result, equalToBool(YES));
+}
+
+- (void)test_one_tonne_is_one_megagram {
+    MKQuantity *expected = [MKQuantity mass_megagramWithAmount:@1];
+    BOOL result = [[MKQuantity mass_tonneWithAmount:@1]isTheSame:expected];
+    
+    assertThatBool(result, equalToBool(YES));
+}
+
+@end
