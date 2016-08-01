72d0e391796a2cd96de1244a75e6c94268f69cc2
Michal Konturek <michal.konturek@gmail.com>
2016-08-01 15:07:41 +0100

    Adds implementation for EnergyUnit

--- Example/Tests/EnergyUnitTests.swift
+++ Example/Tests/EnergyUnitTests.swift
@@ -12,4 +12,27 @@ import XCTest
 
 class EnergyUnitTests: XCTestCase {
 
+    func test_correctness() {
+        XCTAssertTrue(1.gigajoule() == 1000.megajoule())
+        XCTAssertTrue(1.megajoule() == 1000.kilojoule())
+        XCTAssertTrue(1.kilojoule() == 1000.joule())
+        XCTAssertTrue(1.kilocalorie() == 1000.calorie())
+
+        let joule = NSDecimalNumber(string: "4186.7999408823848347407261334609")
+        XCTAssertTrue(joule.joule() == 1.kilocalorie())
+    }
+    
+    func test_extension() {
+        self.assert(1.gigajoule(), expectedAmount: 1, expectedUnit: EnergyUnit.gigajoule)
+        self.assert(1.5.megajoule(), expectedAmount: 1.5, expectedUnit: EnergyUnit.megajoule)
+        self.assert(0.00001.kilojoule(), expectedAmount: 0.00001, expectedUnit: EnergyUnit.kilojoule)
+        self.assert(1.joule(), expectedAmount: 1, expectedUnit: EnergyUnit.joule)
+        self.assert(1.kilocalorie(), expectedAmount: 1, expectedUnit: EnergyUnit.kilocalorie)
+        self.assert(1.calorie(), expectedAmount: 1, expectedUnit: EnergyUnit.calorie)
+    }
+    
+    func assert(item: Quantity, expectedAmount: NSNumber, expectedUnit: Unit) {
+        XCTAssertEqual(item.amount, expectedAmount)
+        XCTAssertEqual(item.unit, expectedUnit)
+    }
 }
--- MKUnits/Classes/EnergyUnit.swift
+++ MKUnits/Classes/EnergyUnit.swift
@@ -7,3 +7,81 @@
 //
 
 import Foundation
+
+public final class EnergyUnit: Unit {
+    
+    static var gigajoule: EnergyUnit {
+        return EnergyUnit(
+            name: "gigajoule",
+            symbol: "GJ",
+            ratio: NSDecimalNumber(mantissa: 2388459, exponent: 2, isNegative: false)
+        )
+    }
+    
+    static var megajoule: EnergyUnit {
+        return EnergyUnit(
+            name: "megajoule",
+            symbol: "MJ",
+            ratio: NSDecimalNumber(mantissa: 2388459, exponent: -1, isNegative: false)
+        )
+    }
+    
+    static var kilojoule: EnergyUnit {
+        return EnergyUnit(
+            name: "kilojoule",
+            symbol: "kJ",
+            ratio: NSDecimalNumber(mantissa: 2388459, exponent: -4, isNegative: false)
+        )
+    }
+    
+    static var joule: EnergyUnit {
+        return EnergyUnit(
+            name: "joule",
+            symbol: "J",
+            ratio: NSDecimalNumber(mantissa: 2388459, exponent: -7, isNegative: false)
+        )
+    }
+    
+    static var kilocalorie: EnergyUnit {
+        return EnergyUnit(
+            name: "kilocalorie",
+            symbol: "kcal",
+            ratio: NSDecimalNumber(mantissa: 1, exponent: 3, isNegative: false)
+        )
+    }
+
+    static var calorie: EnergyUnit {
+        return EnergyUnit(
+            name: "calorie",
+            symbol: "cal",
+            ratio: NSDecimalNumber.one()
+        )
+    }
+}
+
+public extension NSNumber {
+    
+    public func gigajoule() -> Quantity {
+        return Quantity(amount: self, unit: EnergyUnit.gigajoule)
+    }
+    
+    public func megajoule() -> Quantity {
+        return Quantity(amount: self, unit: EnergyUnit.megajoule)
+    }
+    
+    public func kilojoule() -> Quantity {
+        return Quantity(amount: self, unit: EnergyUnit.kilojoule)
+    }
+    
+    public func joule() -> Quantity {
+        return Quantity(amount: self, unit: EnergyUnit.joule)
+    }
+    
+    public func kilocalorie() -> Quantity {
+        return Quantity(amount: self, unit: EnergyUnit.kilocalorie)
+    }
+    
+    public func calorie() -> Quantity {
+        return Quantity(amount: self, unit: EnergyUnit.calorie)
+    }
+}
